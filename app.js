5b771ea750dfa5c63b11875785de625df3e4f007
Josie <tinaweizq@outlook.com>
2023-09-04 12:09:58 -0700

    level 1 auth

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 //jshint esversion:6
 import express from "express";
 import ejs from "ejs";
-import 
+import mongoose from "mongoose";
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
+const userSchema = {
+    email: String,
+    password: String
+}
+
+const User = new mongoose.model("User", userSchema);
+
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -20,6 +30,41 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
     res.render("register");
 })
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    })
+
+    newUser.save()
+        .then(() => res.render("secrets"))
+        .catch((err) => console.log(err))
+})
+
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username})
+        .then((foundUser) => {
+            if (foundUser) {
+                if (foundUser.password === password) {
+                    res.render("secrets");
+                } else {
+                    console.log("Wrong password");
+                    res.redirect("/login");
+                }
+            } else {
+                console.log("User don't exist");
+                res.redirect("/login");
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
