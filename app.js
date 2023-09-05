811997f92668fdc6b48b8ac9c096f9d5d19bbf2a
Zhouqi Wei <122337990+JosieWei2023@users.noreply.github.com>
2023-09-05 15:35:18 -0700

    level 4 salting and hashing password with bcrypt

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ import express from "express";
 import ejs from "ejs";
 import mongoose from "mongoose";
 /*import encrypt from "mongoose-encryption";*/
-import md5 from "md5";
+/*import md5 from "md5";*/
+import bcrypt from "bcrypt";
 
+const saltRounds = 10;
 const app = express();
 const port = 3000;
 
@@ -39,29 +41,35 @@ app.get("/register", (req, res) => {
 })
 
 app.post("/register", (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
 
-    newUser.save()
-        .then(() => res.render("secrets"))
-        .catch((err) => console.log(err))
+        newUser.save()
+            .then(() => res.render("secrets"))
+            .catch((err) => console.log(err))
+    });
 })
 
 app.post("/login", (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username})
         .then((foundUser) => {
             if (foundUser) {
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                } else {
-                    console.log("Wrong password");
-                    res.redirect("/login");
-                }
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    // result === true
+                    if (result === true) {
+                        res.render("secrets");
+                    } else {
+                        console.log("Wrong password");
+                        res.redirect("/login");
+                    }
+                });
             } else {
                 console.log("User don't exist");
                 res.redirect("/login");
